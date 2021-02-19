81e3d8f64f8cb9142eabe0ffbfe58473e9686626
alex.sharov <AskAlexSharov@gmail.com>
2021-02-19 22:14:55 +0700

    squash

--- eth/stagedsync/stage_execute.go
+++ eth/stagedsync/stage_execute.go
@@ -156,8 +156,9 @@ func SpawnExecuteBlocksStage(s *StageState, stateDB ethdb.Database, chainConfig 
 	var batch ethdb.DbWithPendingMutations
 	useBatch := !useSilkworm && params.Cache == nil
 	if useBatch {
-		batch = tx.NewBatch()
-		defer batch.Rollback()
+		batch = tx
+		//batch = tx.NewBatch()
+		//defer batch.Rollback()
 	}
 	if !useSilkworm && params.Cache != nil {
 		batch = tx
@@ -206,9 +207,9 @@ func SpawnExecuteBlocksStage(s *StageState, stateDB ethdb.Database, chainConfig 
 					return err
 				}
 				if useBatch {
-					if err = batch.CommitAndBegin(context.Background()); err != nil {
-						return err
-					}
+					//if err = batch.CommitAndBegin(context.Background()); err != nil {
+					//	return err
+					//}
 				}
 				if !useExternalTx {
 					if err = tx.CommitAndBegin(context.Background()); err != nil {
@@ -260,9 +261,9 @@ func SpawnExecuteBlocksStage(s *StageState, stateDB ethdb.Database, chainConfig 
 			if err := s.Update(batch, stageProgress); err != nil {
 				return err
 			}
-			if _, err := batch.Commit(); err != nil {
-				return fmt.Errorf("%s: failed to write batch commit: %v", logPrefix, err)
-			}
+			//if _, err := batch.Commit(); err != nil {
+			//	return fmt.Errorf("%s: failed to write batch commit: %v", logPrefix, err)
+			//}
 		}
 		if !useExternalTx {
 			if _, err := tx.Commit(); err != nil {
