e823257778b51f0ba9e6840c40bbaed2ae888a4d
alex.sharov <AskAlexSharov@gmail.com>
2021-02-19 21:06:22 +0700

    squash

--- eth/stagedsync/stage_execute.go
+++ eth/stagedsync/stage_execute.go
@@ -85,9 +85,9 @@ func executeBlockWithGo(block *types.Block, tx ethdb.DbWithPendingMutations, cac
 	if params.WriterBuilder != nil {
 		stateWriter = params.WriterBuilder(batch, tx, blockNum)
 	} else if cache == nil {
-		stateWriter = state.NewPlainStateWriter(batch, batch, blockNum)
+		stateWriter = state.NewPlainStateWriter(batch, tx, blockNum)
 	} else {
-		stateWriter = state.NewCachedWriter(state.NewChangeSetWriterPlain(batch, blockNum), cache)
+		stateWriter = state.NewCachedWriter(state.NewChangeSetWriterPlain(tx, blockNum), cache)
 	}
 
 	engine := chainContext.Engine()
@@ -99,7 +99,7 @@ func executeBlockWithGo(block *types.Block, tx ethdb.DbWithPendingMutations, cac
 	}
 
 	if params.WriteReceipts {
-		if err = rawdb.AppendReceipts(batch, blockNum, receipts); err != nil {
+		if err = rawdb.AppendReceipts(tx, blockNum, receipts); err != nil {
 			return err
 		}
 	}
@@ -156,9 +156,8 @@ func SpawnExecuteBlocksStage(s *StageState, stateDB ethdb.Database, chainConfig 
 	var batch ethdb.DbWithPendingMutations
 	useBatch := !useSilkworm && params.Cache == nil
 	if useBatch {
-		batch = tx
-		//batch = tx.NewBatch()
-		//defer batch.Rollback()
+		batch = tx.NewBatch()
+		defer batch.Rollback()
 	}
 	if !useSilkworm && params.Cache != nil {
 		batch = tx
@@ -207,9 +206,9 @@ func SpawnExecuteBlocksStage(s *StageState, stateDB ethdb.Database, chainConfig 
 					return err
 				}
 				if useBatch {
-					//if err = batch.CommitAndBegin(context.Background()); err != nil {
-					//	return err
-					//}
+					if err = batch.CommitAndBegin(context.Background()); err != nil {
+						return err
+					}
 				}
 				if !useExternalTx {
 					if err = tx.CommitAndBegin(context.Background()); err != nil {
@@ -261,9 +260,9 @@ func SpawnExecuteBlocksStage(s *StageState, stateDB ethdb.Database, chainConfig 
 			if err := s.Update(batch, stageProgress); err != nil {
 				return err
 			}
-			//if _, err := batch.Commit(); err != nil {
-			//	return fmt.Errorf("%s: failed to write batch commit: %v", logPrefix, err)
-			//}
+			if _, err := batch.Commit(); err != nil {
+				return fmt.Errorf("%s: failed to write batch commit: %v", logPrefix, err)
+			}
 		}
 		if !useExternalTx {
 			if _, err := tx.Commit(); err != nil {
